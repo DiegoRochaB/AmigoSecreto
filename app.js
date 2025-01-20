931f8ecba6a26a28e11bba3eaab4f30ce5d9e73d
Diego <arreleb@gmail.com>
2025-01-20 13:09:48 -0300

    Add files via upload
    Added list of names

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let listaNomes = [];
+let amigos = [];
 let inputName = document.getElementById("amigo");
 let clearInput = "";
+let lista = document.getElementById("listaAmigos")
 
 function adicionarAmigo(){
     if (inputName.value == ""){
         alert("Por favor insira um nome");
     } else {
-        listaNomes.push(inputName.value);
+        amigos.push(inputName.value);
         inputName.value = clearInput;
-        console.log(listaNomes);
+        console.log(amigos);
     }
 }
+
+function listaDeAmigos() {
+    if (lista.children.length === 0) {
+    } else {
+      lista.innerHTML = '';
+    }
+    
+    for (let i = 0; i < amigos.length; i++) {
+      lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+  }
+
+/*
+function listaDeAmigos (){
+    lista.innerHTML = '';
+    for (let i = 0; i < amigos.length; i++){
+        lista.innerHTML = `<li>${amigos[i]}</li>`;
+    }
+}
+*/
+//lista.innerHTML += "";
