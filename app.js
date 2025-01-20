f6e3c1430665ffc8ec4d49d29e2f14c6a0ddf639
Diego <arreleb@gmail.com>
2025-01-20 08:39:55 -0300

    Add files via upload
    Added function for name list and validation for user input (if it's empty, not security validation)

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaNomes = [];
+let inputName = document.getElementById("amigo");
+let clearInput = "";
+
+function adicionarAmigo(){
+    if (inputName.value == ""){
+        alert("Por favor insira um nome");
+    } else {
+        listaNomes.push(inputName.value);
+        inputName.value = clearInput;
+        console.log(listaNomes);
+    }
+}
